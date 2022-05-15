d40b77a70c50be941a7f9d8637ce08c992cd59c3
Mircea Taras <rikoru1@gmail.com>
2022-05-15 01:17:51 -0700

    Created initial nav - incorrect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
 
+const sections = [
+  {
+    name: "About",
+    url: "/about"
+  },
+  {
+    name: "Projects",
+    url: "/projects"
+  }
+]
+
+const innerSection = (props) => (
+  <li className="inner-section">
+    <a href={props.url}>{props.name}</a>
+  </li>
+);
+
+// TODO: Make proper nav https://developer.mozilla.org/en-US/docs/Web/HTML/Element/nav
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Mircea's Personal Site (WIP)
+        <h1>Mircea's Personal Site (WIP)</h1>
+        <div className="nav-bar">
+          <ul>{sections.map(innerSection)}</ul>
+        </div>
       </header>
     </div>
   );
